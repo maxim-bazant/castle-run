703ec0d43b0b0adcbc3da4375d52e0a459ad96b2
Maxim Bazant <maxim.bazant@outlook.com>
2020-04-11 17:30:20 +0200

    added images

--- game.py
+++ game.py
@@ -2,11 +2,6 @@ import pygame
 
 pygame.init()
 
-x = 0
-y = 0
-width = 75
-height = 75
-
 # init variables
 win_width, win_height = 800, 600
 
@@ -28,19 +23,9 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        x -= 3
-    if keys[pygame.K_RIGHT]:
-        x += 3
-    if keys[pygame.K_UP]:
-        y -= 3
-    if keys[pygame.K_DOWN]:
-        y += 3
-
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    player.show_me()
     pygame.display.update()
-
     clock.tick(FPS)
 
 pygame.quit()
