1f05c4da34f4a3414d32dfe084cc06326ac4e550
Maxim Bazant <maxim.bazant@outlook.com>
2020-04-11 22:26:59 +0200

    new images and new code

--- game.py
+++ game.py
@@ -3,19 +3,6 @@
 import pygame
 pygame.init()
 
-
-class Background(object):
-    def __init__(self, image):
-        self.x = 0
-        self.y = 0
-        self.image = image
-        self.width = self.image.get_rect().width
-        self.height = self.image.get_rect().height
-
-    def show_and_move_layer(self):
-        pass
-
-
 win_width, win_height = 800, 600
 win = pygame.display.set_mode((win_width, win_height))
 win_caption = pygame.display.set_caption("test1")
@@ -25,7 +12,7 @@ running = True
 clock = pygame.time.Clock()
 
 FPS = 30
-
+# main loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
