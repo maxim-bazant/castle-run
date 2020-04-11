9b2d1c313be0ad7ccc150061da87e9e4a41bfc53
Maxim Bazant <maxim.bazant@outlook.com>
2020-04-11 18:55:07 +0200

    new images and new code

--- game.py
+++ game.py
@@ -1,10 +1,22 @@
-import pygame
+#  tutorial 2 - background scrolling
 
+import pygame
 pygame.init()
 
-# init variables
-win_width, win_height = 800, 600
 
+class Background(object):
+    def __init__(self, image):
+        self.x = 0
+        self.y = 0
+        self.image = image
+        self.width = self.image.get_rect().width
+        self.height = self.image.get_rect().height
+
+    def show_and_move_layer(self):
+        pass
+
+
+win_width, win_height = 800, 600
 win = pygame.display.set_mode((win_width, win_height))
 win_caption = pygame.display.set_caption("test1")
 
@@ -12,19 +24,13 @@ running = True
 
 clock = pygame.time.Clock()
 
-FPS = 240
+FPS = 30
 
-# main loop
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
-
-    win.fill((0, 0, 0))
-    player.show_me()
     pygame.display.update()
     clock.tick(FPS)
 
