18826c2f46afb369acef385bdf46b25c8f9a5215
Maxim Bazant <maxim.bazant@outlook.com>
2020-04-11 09:07:18 +0200

    clean up

--- game.py
+++ game.py
@@ -2,6 +2,10 @@ import pygame
 
 pygame.init()
 
+x = 0
+y = 0
+width = 75
+height = 75
 
 # init variables
 win_width, win_height = 800, 600
@@ -13,7 +17,7 @@ running = True
 
 clock = pygame.time.Clock()
 
-FPS = 30
+FPS = 240
 
 # main loop
 while running:
@@ -22,6 +26,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= 3
+    if keys[pygame.K_RIGHT]:
+        x += 3
+    if keys[pygame.K_UP]:
+        y -= 3
+    if keys[pygame.K_DOWN]:
+        y += 3
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
 
     clock.tick(FPS)
