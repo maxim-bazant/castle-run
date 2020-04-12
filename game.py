01271a7d0214b83b49ef1c0985315718bc2fa2b9
Maxim Bazant <maxim.bazant@outlook.com>
2020-04-12 11:41:44 +0200

    layer_scrolling

--- game.py
+++ game.py
@@ -15,6 +15,41 @@ running = True
 clock = pygame.time.Clock()
 
 FPS = 30
+
+
+class Layer(object):
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.image = pygame.image.load("images/layer.png")
+        self.width = self.image.get_rect().width
+        self.height = self.image.get_rect().height
+
+    def show_me(self):
+        win.blit(self.image, (self.x, self.y))
+
+    @staticmethod
+    def layer_scrolling(l_1, l_2):
+        l_1.x += -3
+        l_2.x += -3
+
+        l_1.show_me()
+        l_2.show_me()
+
+        if l_1.x < -2 * l_1.width + win_width:
+            l_1.x = win_width
+        if l_2.x < -2 * l_1.width + win_width:
+            l_2.x = win_width
+
+
+# layer variables
+layer_1 = Layer()
+layer_2 = Layer()
+
+layer_2.x = layer_2.width
+
+
 # main loop
 while running:
     for event in pygame.event.get():
@@ -22,6 +57,7 @@ while running:
             running = False
 
     win.blit(background, (0, 0))
+    Layer.layer_scrolling(layer_1, layer_2)
     pygame.display.update()
     clock.tick(FPS)
 
