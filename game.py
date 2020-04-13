e755ff12d1b81587a4e1df4d0ab82821cf0006d4
Maxim Bazant <maxim.bazant@outlook.com>
2020-04-13 12:33:21 +0200

    layer_scrolling

--- game.py
+++ game.py
@@ -4,9 +4,8 @@ import pygame
 pygame.init()
 
 # init variables
-background = pygame.image.load("images/background.png")
 
-win_width, win_height = background.get_rect().width, background.get_rect().height
+win_width, win_height = 1100, 500
 win = pygame.display.set_mode((win_width, win_height))
 win_caption = pygame.display.set_caption("castle_run")
 
@@ -19,25 +18,20 @@ FPS = 30
 
 # layer class
 class Layer(object):
-
-    def __init__(self):
+    def __init__(self, image):
         self.x = 0
         self.y = 0
-        self.image = pygame.image.load("images/layer.png")
+        self.image = image
         self.width = self.image.get_rect().width
         self.height = self.image.get_rect().height
-        self.vel = 3
-
-    def show_me(self):
-        win.blit(self.image, (self.x, self.y))
 
     @staticmethod
-    def layer_scrolling(l_1, l_2):
-        l_1.x -= Layer().vel
-        l_2.x -= Layer().vel
+    def layer_scrolling(l_1, l_2, l_vel):
+        l_1.x -= l_vel
+        l_2.x -= l_vel
 
-        l_1.show_me()
-        l_2.show_me()
+        win.blit(l_1.image, (l_1.x, l_1.y))
+        win.blit(l_2.image, (l_2.x, l_2.y))
 
         if l_1.x < -2 * l_1.width + win_width:
             l_1.x = win_width
@@ -46,10 +40,17 @@ class Layer(object):
 
 
 # layer variables
-layer_1 = Layer()
-layer_2 = Layer()
+layer_1 = Layer(pygame.image.load("images/layer.png"))
+layer_2 = Layer(pygame.image.load("images/layer.png"))
 
+layer_vel = 5
 layer_2.x = layer_2.width
+# background variables
+background_1 = Layer(pygame.image.load("images/background.png"))
+background_2 = Layer(pygame.image.load("images/background.png"))
+
+background_vel = 0.5
+background_2.x = background_2.width
 
 
 # main loop
@@ -58,8 +59,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    win.blit(background, (0, 0))
-    Layer.layer_scrolling(layer_1, layer_2)
+    Layer.layer_scrolling(background_1, background_2, background_vel)
+    Layer.layer_scrolling(layer_1, layer_2, layer_vel)
     pygame.display.update()
     clock.tick(FPS)
 
